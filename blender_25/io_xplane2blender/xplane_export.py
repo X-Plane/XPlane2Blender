5fd4a0186ad8c98fab947a53a57912e9a5b4d9f8
Ondrej <info@anzui.de>
2010-12-08 23:18:57 +0100

    method name change: windowManager.add_fileselect is now fileselect_add This makes exporting using the export dialogue possible again in recent blender builds.

--- blender_25/io_xplane2blender/xplane_export.py
+++ blender_25/io_xplane2blender/xplane_export.py
@@ -1086,5 +1086,5 @@ class ExportXPlane9(bpy.types.Operator):
 
     def invoke(self, context, event):
         wm = context.window_manager
-        wm.add_fileselect(self)
+        wm.fileselect_add(self)
         return {'RUNNING_MODAL'}
