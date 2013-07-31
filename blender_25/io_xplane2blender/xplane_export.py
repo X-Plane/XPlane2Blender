669a06ef8516c675efcd2ee5403018d95e192c3a
Ondrej Brinkel <info@anzui.de>
2013-07-31 15:25:54 +0200

    bugfix in mesh debugging

--- blender_25/io_xplane2blender/xplane_export.py
+++ blender_25/io_xplane2blender/xplane_export.py
@@ -124,7 +124,7 @@ class XPlaneMesh():
                 pass
             for d in self.debug:
                 tris_to_quads = 1.0
-                if d['faces'] > 0:
+                if d['faces'] > 0 and d['obj_faces']:
                   tris_to_quads = d['obj_faces'] / d['faces']
                 debugger.debug('%s: faces %d | obj-faces %d | tris-to-quads ratio %6.2f | indices %d | vertices %d' % (d['name'],d['faces'],d['obj_faces'],tris_to_quads,d['end_index']-d['start_index'],d['vertices']))
             debugger.debug('POINT COUNTS: faces %d - vertices %d - indices %d' % (len(self.faces),len(self.vertices),len(self.indices)))
