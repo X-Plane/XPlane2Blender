38f8bbc99436961e5f06809dfb55fa2be0d45539
Ondrej Brinkel <info@anzui.de>
2015-06-11 21:59:36 +0200

    tests.py now accepts a regex filter for test cases

--- tests.py
+++ tests.py
@@ -3,8 +3,7 @@ import glob
 import subprocess
 import sys
 import shutil
-
-blenderExecutable = 'blender'
+import re
 
 if os.path.exists('./tests/tmp'):
     # empty temp directory
@@ -13,13 +12,36 @@ if os.path.exists('./tests/tmp'):
 # create temp dir if not exists
 os.mkdir('./tests/tmp')
 
-if len(sys.argv) > 1:
-    blenderExecutable = sys.argv[1]
+def getOption(name, default):
+    index = -1
+
+    try:
+        index = sys.argv.index(name)
+    except:
+        pass
+
+    if index > 0 and len(sys.argv) > index + 1:
+        return sys.argv[index + 1]
+
+    return default
+
+filter = getOption('--filter', None)
+blenderExecutable = getOption('--blender', 'blender')
+
+def inFilter(file):
+    if filter == None:
+        return True
+
+    return (re.search(filter, file))
 
 for pyFile in glob.glob('./tests/**/*.test.py'):
-    blendFile = pyFile.replace('.py', '.blend')
+    # skip files not within filter
+    if inFilter(pyFile):
+        blendFile = pyFile.replace('.py', '.blend')
 
-    if os.path.exists(blendFile):
-        subprocess.call([blenderExecutable, '--addons', 'io_xplane2blender', '--factory-startup', '-noaudio', '-b', blendFile, '--python', pyFile])
-    else:
-        subprocess.call([blenderExecutable, '--addons', 'io_xplane2blender', '--factory-startup', '-noaudio', '-b', '--python', pyFile])
+        print('Running file %s' % pyFile)
+
+        if os.path.exists(blendFile):
+            subprocess.call([blenderExecutable, '--addons', 'io_xplane2blender', '--factory-startup', '-noaudio', '-b', blendFile, '--python', pyFile])
+        else:
+            subprocess.call([blenderExecutable, '--addons', 'io_xplane2blender', '--factory-startup', '-noaudio', '-b', '--python', pyFile])
