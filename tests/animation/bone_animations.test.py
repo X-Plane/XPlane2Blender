1caa4a175a8f86e78aa6a4f9be715e8b12e5d1a0
Ondrej Brinkel <info@anzui.de>
2015-09-22 16:00:38 +0200

    added test case for nested bone animations

--- tests/animation/bone_animations.test.py
+++ tests/animation/bone_animations.test.py
@@ -12,13 +12,24 @@ class TestBoneAnimations(XPlaneTestCase):
 
     def test_bone_animations(self):
         def filterLines(line):
-            return isinstance(line[0], str) and (line[0] == 'VT' or line[0].find('TEXTURE') == 0)
+            return isinstance(line[0], str) and (line[0] == 'VT' or line[0].find('ANIM') == 0)
 
         filename = 'test_bone_animations'
         self.assertLayerExportEqualsFixture(
             0, os.path.join(__dirname__, 'fixtures', filename + '.obj'),
-            filename #,
-            # filterLines
+            filename,
+            filterLines
+        )
+
+    def test_nested_bone_animations(self):
+        def filterLines(line):
+            return isinstance(line[0], str) and (line[0] == 'VT' or line[0].find('ANIM') == 0)
+
+        filename = 'test_nested_bone_animations'
+        self.assertLayerExportEqualsFixture(
+            1, os.path.join(__dirname__, 'fixtures', filename + '.obj'),
+            filename,
+            filterLines
         )
 
 runTestCases([TestBoneAnimations])
