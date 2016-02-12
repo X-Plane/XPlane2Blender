58d62305779b2af823c6a01cdd89686969fb7ac9
Ondrej Brinkel <info@anzui.de>
2016-02-12 11:38:17 +0100

    fixed tests.py for python 3.x

--- tests.py
+++ tests.py
@@ -86,6 +86,9 @@ for root, dirs, files in os.walk('./tests'):
 
                 out = subprocess.check_output(args, stderr = subprocess.STDOUT)
 
+                if sys.version_info >= (3, 0):
+                    out = out.decode('utf-8')
+
                 print(out)
 
                 # tests raised an error
