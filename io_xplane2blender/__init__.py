9ae116fbbfdd278b6cf1d62ae5d71c1ee38b001e
Ondrej Brinkel <info@anzui.de>
2016-05-24 20:22:57 +0200

    v3.3.11

--- io_xplane2blender/__init__.py
+++ io_xplane2blender/__init__.py
@@ -24,7 +24,7 @@
 bl_info = {
     "name": "Import/Export: XPlane",
     "author": "Ondrej Brinkel",
-    "version": (3,3,10),
+    "version": (3,3,11),
     "blender": (2, 7, 6),
     "api": 36273,
     "location": "File > Import/Export > XPlane",
