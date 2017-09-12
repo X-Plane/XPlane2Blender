56d4272466293d8882c102dfc9e4b6a1723aaa7b
tngreene <theodorengreene@gmail.com>
2017-09-12 18:06:10 -0400

    New unit test for testing if obj footer contains the version string

---
+++ tests/build_system/build_number_obj_footer.test.py
@@ -0,0 +1,30 @@
+import bpy
+import os
+import re
+import sys
+from io_xplane2blender.tests import *
+from io_xplane2blender import xplane_config
+from io_xplane2blender.xplane_helpers import VerStruct
+
+__dirname__ = os.path.dirname(__file__)
+
+#TEMPLATE_INFO filter obj output. Define above or in the class level 
+#def filterLines(line):
+    #return isinstance(line[0],str) and...
+    
+class TestBlendBuildNumberObjFooter(XPlaneTestCase):
+    def test_build_number_obj_footer(self):
+        bpy.ops.scene.add_xplane_layers()
+        out = self.exportLayer(0)
+        print(out)
+        #import sys;sys.path.append(r'C:\Users\Ted\.p2\pool\plugins\org.python.pydev_5.7.0.201704111357\pysrc')
+        #import pydevd;pydevd.settrace()
+        version_match = re.search("Exported with XPlane2Blender (.*)", out)
+        self.assertTrue(version_match is not None, "Version string not found in footer of obj")
+        
+        version = VerStruct.parse_version(version_match.group(1))
+        self.assertTrue(version is not None, "%s could not be parsed to a valid VerStruct" % version_match.group(1))
+        self.assertTrue(VerStruct.cmp(version,bpy.context.scene.xplane.xplane2blender_ver,True,True) == 0,
+                        "Version in obj is not equal to current version")
+        
+runTestCases([TestBlendBuildNumberObjFooter])
