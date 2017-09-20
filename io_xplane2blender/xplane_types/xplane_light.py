fb7650bc752d180bb10ccd2f4917f7fb96b4cb80
Ben Supnik <ben@x-plane.com>
2017-09-20 15:06:00 -0400

    bug fix: possible fix for #264 - baked animation on lights implemented.
    We didn’t have any code to handle bake matrices on lights ,and the new
    animation code optimizes rotations so now we actually have them.
    
    This should output enough code for the MD-80 to work.

--- io_xplane2blender/xplane_types/xplane_light.py
+++ io_xplane2blender/xplane_types/xplane_light.py
@@ -1,6 +1,8 @@
 from .xplane_object import XPlaneObject
-from ..xplane_helpers import floatToStr
+from ..xplane_helpers import floatToStr, FLOAT_PRECISION, logger
 from ..xplane_constants import *
+from ..xplane_config import getDebug
+import math
 
 # Class: XPlaneLight
 # A Light
@@ -67,21 +69,95 @@ class XPlaneLight(XPlaneObject):
 
         self.getWeight(10000)
 
+    # COPY PASTA WARNING!!!
+    #
+    # This is stolen from the bone code's bake matrix exporter.  I wanted this copied out
+    # to 1. avoid re-test late in beta and 2. to have the option to optimize this later
+    # for lights that can have direction vectors.
+
+    def _writeStaticRotationForLight(self, bakeMatrix):
+        debug = getDebug()
+        indent = self.xplaneBone.getIndent()
+        o = ''
+        bakeMatrix = bakeMatrix
+        rotation = bakeMatrix.to_euler('XYZ')
+        rotation[0] = round(rotation[0],5)
+        rotation[1] = round(rotation[1],5)
+        rotation[2] = round(rotation[2],5)
+        
+        # ignore noop rotations
+        if rotation[0] == 0 and rotation[1] == 0 and rotation[2] == 0:
+            return o
+
+        if debug:
+            o += indent + '# static rotation\n'
+
+		# Ben says: this is SLIGHTLY counter-intuitive...Blender axes are
+		# globally applied in a Euler, so in our XYZ, X is affected -by- Y
+		# and both are affected by Z.
+		#
+		# Since X-Plane works opposite this, we are going to apply the
+		# animations exactly BACKWARD! ZYX.  The order here must
+		# be opposite the decomposition order above.
+		#
+		# Note that since our axis naming is ALSO different this will
+		# appear in the OBJ file as Y -Z X.
+		#
+		# see also: http://hacksoflife.blogspot.com/2015/11/blender-notepad-eulers.html
+
+        axes = (2, 1, 0)
+        eulerAxes = [(0.0,0.0,1.0),(0.0,1.0,0.0),(1.0,0.0,0.0)]
+        i = 0
+
+        for axis in eulerAxes:
+            deg = math.degrees(rotation[axes[i]])
+
+            # ignore zero rotation
+            if not deg == 0:
+                o += indent + 'ANIM_rotate\t%s\t%s\t%s\t%s\t%s\n' % (
+                    floatToStr(axis[0]),
+                    floatToStr(axis[2]),
+                    floatToStr(-axis[1]),
+                    floatToStr(deg), floatToStr(deg)
+                )
+
+            i += 1
+
+        return o
+
+
     def write(self):
         indent = self.xplaneBone.getIndent()
         o = super(XPlaneLight, self).write()
 
         bakeMatrix = self.xplaneBone.getBakeMatrixForAttached()
-
         translation = bakeMatrix.to_translation()
-		# TODO: the bake matrix may have a rotation component.
-		# in this case we need to emit static rotations to get the light
-		# pointing in the right direction.
-		#
-		# Ideally we'd know whether the light has some kind of symetry (E.g.
-		# axial around a given axis for Z or omni) and throw out unnecessary
-		# transforms, since they are toxic to instancing.
-
+        rotation = bakeMatrix.to_euler('XYZ')
+        
+        rotation[0] = round(rotation[0],5)
+        rotation[1] = round(rotation[1],5)
+        rotation[2] = round(rotation[2],5)
+        
+        has_anim = False
+        
+        # Ben says: lights always have some kind of offset because the light itself
+        # is "at" 0,0,0, so we treat the translation as the light position.
+        # But if there is a ROTATION then in the light's bake matrix, the
+        # translation is pre-rotation.  but we want to write a single static rotation
+        # and then NOT write a translation every time.
+        #
+        # Soooo... we write a bake matrix and then we transform the translation by the
+        # inverse to change our animation order (so we really have rot, trans when we
+        # originally had trans, rot) and now we can use the translation in the lamp
+        # itself.
+        
+        if rotation[0] != 0.0 or rotation[1] != 0.0 or rotation[2] != 0.0:
+            rot_matrix = rotation.to_matrix().to_4x4()
+            o += "%sANIM_begin\n" % indent
+            o += self._writeStaticRotationForLight(rot_matrix)
+            translation = rot_matrix.inverted() * translation
+            has_anim = True
+        
         if self.lightType == LIGHT_NAMED:
             o += "%sLIGHT_NAMED\t%s %s %s %s\n" % (
                 indent, self.lightName,
@@ -120,5 +196,8 @@ class XPlaneLight(XPlaneObject):
             offset = self.indices[0]
             count = self.indices[1] - self.indices[0]
             o += "%sLIGHTS\t%d %d\n" % (indent, offset, count)
+
+        if has_anim:
+            o += "%sANIM_end\n" % indent
 
         return o
