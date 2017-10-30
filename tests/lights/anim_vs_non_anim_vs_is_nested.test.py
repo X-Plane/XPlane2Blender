e2b2a0df2473260fb26e692bf088895e03177869
tngreene <theodorengreene@gmail.com>
2017-10-30 11:51:37 -0400

    Adds new unit test testing the relationship between animation and parenthood

---
+++ tests/lights/anim_vs_non_anim_vs_is_nested.test.py
@@ -0,0 +1,26 @@
+import bpy
+import os
+import inspect
+import sys
+
+from io_xplane2blender.tests import *
+from io_xplane2blender import xplane_config
+
+__dirname__ = os.path.dirname(__file__)
+
+def filterLines(line):
+    return isinstance(line[0],str) and\
+            ("ANIM" in line[0] or\
+            "LIGHT" in line[0])
+
+class TestAnimVsNonAnimVsIsNested(XPlaneTestCase):
+    def test_anim_vs_non_anim_vs_has_parent(self):
+        filename = inspect.stack()[0][3]
+
+        self.assertLayerExportEqualsFixture(
+            0, os.path.join(__dirname__, 'fixtures', filename + '.obj'),
+            filename,
+            filterLines
+        )
+
+runTestCases([TestAnimVsNonAnimVsIsNested])
