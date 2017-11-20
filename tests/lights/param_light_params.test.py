1ca84abf8277b6d553f01c382ffe99c103dd4865
tngreene <theodorengreene@gmail.com>
2017-11-20 14:34:45 -0500

    Fixes unit test for light params comment. In the .blend file an object name (4_...) was changed

--- tests/lights/param_light_params.test.py
+++ tests/lights/param_light_params.test.py
@@ -11,12 +11,19 @@ __dirname__ = os.path.dirname(__file__)
  
 class TestParamLightParams(XPlaneTestCase):
     def test_comment_correct(self):
-        light = xplane_light.XPlaneLight(bpy.data.objects["4_comment contents_preserved_exactly"])
-        light.collect()
-        self.assertTrue(light.comment == "0 spaces, number starts comment with uneven and a   trailing  space ")
+        #COPY-PASTA WARNING from xplane_file: 65-75
+        # What we need is an xplaneFile in the data model and interrupt
+        # the export before the xplane_file gets deleted when going out of scope
+        xplaneLayer = xplane_file.getXPlaneLayerForBlenderLayerIndex(0)
 
-        out  = self.exportLayer(0)
-        self.assertLoggerErrors(0)
+        assert xplaneLayer is not None
+        xplaneFile = xplane_file.XPlaneFile(xplane_file.getFilenameFromXPlaneLayer(xplaneLayer), xplaneLayer)
+
+        assert xplaneFile is not None
+        xplaneFile.collectFromBlenderLayerIndex(0)
+
+        light = xplaneFile.objects["4_comment_contents_preserved_exactly"]
+        self.assertTrue(light.comment == "0 spaces, number starts comment with uneven and a   trailing  space ")
 
     def test_illegal_params_content(self):
         out = self.exportLayer(1)
