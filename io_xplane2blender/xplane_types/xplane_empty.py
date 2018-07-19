a35576bfa03b060f2040d997e851873c3ef340b1
tngreene <theodorengreene@gmail.com>
2018-07-19 19:42:40 -0400

    Phi,theta,psi now get automatically taken from an object's bake matrix! The properties haven't been removed yet, and there is no unit tests or checking that Other animation types work with this.

--- io_xplane2blender/xplane_types/xplane_empty.py
+++ io_xplane2blender/xplane_types/xplane_empty.py
@@ -1,3 +1,5 @@
+import math
+
 import bpy
 from io_xplane2blender import xplane_config
 from io_xplane2blender import xplane_helpers
@@ -25,18 +27,25 @@ class XPlaneEmpty(XPlaneObject):
         if (int(bpy.context.scene.xplane.version) >= 1130 and
                 (empty.special_type == EMPTY_USAGE_EMITTER_PARTICLE or
                 empty.special_type == EMPTY_USAGE_EMITTER_SOUND)):
-            em_location = xplane_helpers.vec_b_to_x(self.blenderObject.location)
+            bake_matrix = self.xplaneBone.getBakeMatrixForAttached()
+            print(bake_matrix)
+            translation = bake_matrix.to_translation()
+            print(translation)
+            em_location = xplane_helpers.vec_b_to_x(translation)
+            #yaw,pitch,roll
+            theta,psi,phi = [math.degrees(comp) for comp in bake_matrix.to_euler()[:]]
             o += 'EMITTER {name} {x} {y} {z} {phi} {theta} {psi}'.format(
                     name=empty.emitter_props.name,
                     x=em_location.x,
                     y=em_location.y,
                     z=em_location.z,
-                    phi=empty.emitter_props.phi,
-                    theta=empty.emitter_props.theta,
-                    psi=empty.emitter_props.psi)
+                    phi=-phi, #yaw right
+                    theta=theta, #pitch up
+                    psi=psi) #roll right
 
             if empty.emitter_props.index > 0:
                 o += ' {}'.format(empty.emitter_props.index)
+            print(o)
 
             o +='\n'
 
