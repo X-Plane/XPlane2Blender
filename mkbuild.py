1b875fafe33883a535e2ea2cca6ccaa933aacca5
tngreene <theodorengreene@gmail.com>
2018-07-30 19:19:01 -0400

    The build script now replaces data and restores it succesfully, running tests is now possible.

--- mkbuild.py
+++ mkbuild.py
@@ -1,8 +1,11 @@
 import argparse
+import datetime
 import os
 import re
 import shutil
 import sys
+import subprocess
+
 from typing import List, Optional, Tuple
 
 from collections import namedtuple
@@ -30,7 +33,10 @@ def _make_parser()->argparse.ArgumentParser:
             type=str,
             choices=["dev","alpha","beta","rc"],
             help="Override CURRENT_BUILD_TYPE")
-    build_metadata_group.add_argument("--data-model-number",
+    build_metadata_group.add_argument("--build-type-version",
+            type=int,
+            help="Override CURRENT_BUILD_TYPE_VERSION")
+    build_metadata_group.add_argument("--data-model-version",
             type=int,
             help="Override CURRENT_DATA_MODEL_VERSION with an arbitrary number (dangerous, only increment!)")
     build_metadata_group.add_argument("--build-number",
@@ -62,14 +68,11 @@ def _make_parser()->argparse.ArgumentParser:
             default="./builds",
             help="Destination for the zip file. Folders will be created if it does not exist, files will be overwritten")
 
-    parser.add_argument("TEST_ARGS",
-            default="",
-            help="Arguments to pass into test suite")
-
     return parser
 
 #parser.parse_args()
 
+#TODO: We need an is_valid, in case the versions tests aren't run, right?
 class VerData():
     '''A small completely mutable version of XPlaneHelper's VerStruct class (so we don't have to worry about importing bpy without Blender'''
     def __init__(self,
@@ -84,8 +87,18 @@ class VerData():
         self.data_model_version = data_model_version   #if data_model_version is not None else 0
         self.build_number       = build_number         #if build_number       is not None else xplane_constants.BUILD_NUMBER_NONE
 
+    @staticmethod
+    def make_new_build_number():
+        #Use the UNIX Timestamp in UTC 
+        return datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y%m%d%H%M%S")
+
+build_types = { "alpha":"BUILD_TYPE_ALPHA",
+                "beta": "BUILD_TYPE_BETA",
+                "dev":  "BUILD_TYPE_DEV",
+                "rc":   "BUILD_TYPE_RC"}
+
 def change_version_info(new_version:VerData)->Optional[VerData]:
-    src_folder = os.path.join(os.path.dirname(__file__),"io_xplane2blender")
+    src_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)),"io_xplane2blender")
     build_folder = src_folder + "_build"
 
     old_version = VerData()
@@ -95,19 +108,14 @@ def change_version_info(new_version:VerData)->Optional[VerData]:
     xplane_config_file = os.path.join(src_folder,"xplane_config.py")
     
     try:
-        out_config_file = []
+        out_config_file_contents = [] # type: str
         with open(xplane_config_file, 'r') as in_config_file:
-            build_types = { "alpha":"BUILD_TYPE_ALPHA",
-                            "beta": "BUILD_TYPE_BETA",
-                            "dev":  "BUILD_TYPE_DEV",
-                            "rc":   "BUILD_TYPE_RC"}
-
             for line in in_config_file:
                 o_line = line
                 if re.match("^CURRENT_BUILD_TYPE ",line):
-                    old_version.build_type = re.search("\.(BUILD_TYPE_.*)",line).group(1)
+                    old_version.build_type = re.search("\.BUILD_TYPE_([A-Z]+)$",line).group(1).lower()
                     if new_version.build_type:
-                        line = re.sub(r"\..+",".BUILD_TYPE_"+new_version.build_type.capitalize(),line)
+                        line = re.sub(r"\.BUILD_TYPE_[A-Z]+$",".BUILD_TYPE_"+new_version.build_type.upper(),line)
 
                 if re.match("^CURRENT_BUILD_TYPE_VERSION ",line):
                     old_version.build_type_version = re.search(r"\d+",line).group(0)
@@ -120,44 +128,38 @@ def change_version_info(new_version:VerData)->Optional[VerData]:
                         line = re.sub(r"\d+",new_version.data_model_version,line)
 
                 if re.match("^CURRENT_BUILD_NUMBER ",line):
-                    old_version.build_number = re.search(r"xplane_constants.*",line).group(0)
+                    search_str = 'xplane_constants.BUILD_NUMBER_NONE|"\d{14}"'
+                    old_version.build_number = re.search(search_str,line).group(0)
                     if new_version.build_number:
-                        line = re.sub(r"xplane_constants.BUILD_NUMBER_NONE",\
-                            "'{}'".format(str(new_version.build_number)),\
+                        line = re.sub(search_str,
+                            '{}'.format(str(new_version.build_number)),\
                             line)
 
                 if line != o_line:
-                    print("o_line " + o_line)
-                    print("line " + line)
-                out_config_file += line
+                    print("o_line " + o_line[:-1])
+                    print("line " + line[:-1])
+                out_config_file_contents.append(line)
 
-        #with open(xplane_config_file, 'w') as out_config_file:
-        #    for line in out_config_file:
-        #        out_config_file.write(line)
+        with open(xplane_config_file, 'w', newline='\n') as out_config_file:
+            for line in out_config_file_contents:
+                out_config_file.write(line)
     except OSError as e:
         print(e)
-        return e
+        return None
     else:
-        print(old_version)
+        #print(old_version)
         return old_version
 
 def main(argv=None):
     exit_code = 0
     if argv is None:
-        argv = _make_parser().parse_args()
+        argv,test_args = _make_parser().parse_known_args()
 
-    print(argv)
-    src_folder = os.path.join(os.path.dirname(__file__),"io_xplane2blender")
+    #TODO: Assert run inside XPlane2Blender folder
+    src_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)),"io_xplane2blender")
     build_folder = src_folder + "_build"
 
     # 1. Delete the old build folder, if present
-    # 2. Parse __init__.py and xplane_config.py for version info, swap new for old
-    # 3. Run any tests desired
-    # 4. Copy the source to create a new build folder
-    # 5. Delete temporary files, gitignore files, and (optionally) untracked files
-    # 6. Zip up folder, rename it, move to ./builds folder
-    # 7. (if desired, which is almost always), replace old values for __init__.py and xplane_config.
-    # This should always be able to happen, no matter what happens between 2 and 7
     try:
         if os.path.isdir(build_folder):
             shutil.rmtree(build_folder)
@@ -168,13 +170,36 @@ def main(argv=None):
     if argv.clean:
         return 1
 
-    old_version = change_version_info(VerData(build_number="20180727043920"))
-    try:
-        shutil.copytree(src_folder,build_folder)
-    except shutil.Error as e:
-        print(e)
+    # 2. Parse __init__.py and xplane_config.py for version info, swap old for new
+    print(argv)
+    old_version = change_version_info(VerData(
+        addon_version=[argv.major,argv.minor,argv.revision],
+        build_type=argv.build_type,
+        build_type_version=str(argv.build_type_version),
+        build_number='"' + VerData.make_new_build_number() + '"'))
+    if not old_version:
         return 1
 
+    #Always make sure no matter what, we replace old values for __init__.py and xplane_config
+    try:
+        # 3. Run any tests desired
+        if argv.test_level is not "none":
+            completed = subprocess.run(["python","tests.py"] + test_args)
+            #print(completed.stdout)
+
+        # 4. Copy the source to create a new build folder
+        try:
+            shutil.copytree(src_folder,build_folder)
+        except shutil.Error as e:
+            print(e)
+            return 1
+
+        # 5. Delete temporary files, gitignore files, and (optionally) untracked files
+        # 6. Zip up folder, rename it, move to ./builds folder
+    finally:
+        # 7. (if desired, which is almost always), replace old values for __init__.py and xplane_config.
+        if not argv.make_overrides_permanent:
+            change_version_info(old_version)
     """
     try:
         shutil.rmtree(src_folder)
