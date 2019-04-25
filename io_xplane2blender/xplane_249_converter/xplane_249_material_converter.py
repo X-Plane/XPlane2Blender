049beae19cf31f6adeb3d25871438949cf5e1d38
tngreene <theodorengreene@gmail.com>
2019-04-24 22:15:57 -0400

    New unit test for one of every face. We now have real 2.79 props being changed! Unfortunatly, not all the buttons are working, so these tests can't be completed yet.

--- io_xplane2blender/xplane_249_converter/xplane_249_material_converter.py
+++ io_xplane2blender/xplane_249_converter/xplane_249_material_converter.py
@@ -50,7 +50,6 @@ def convert_materials(scene: bpy.types.Scene, workflow_type: xplane_249_constant
 
     print([s.name for s in search_objs])
 
-    #
     for search_obj in search_objs:
         print(search_obj.name)
         print("slots:", [s.name for s in search_obj.material_slots])
@@ -61,63 +60,114 @@ def convert_materials(scene: bpy.types.Scene, workflow_type: xplane_249_constant
             #-- TexFace Flags ------------------------------------------------
             # If True, it means this is semantically enabled for export
             # (which is important for TWOSIDE)
-            TEX = _has_TEX(_get_mtface_flag(search_obj.data)) # type: bool
-            PANEL = False # type: bool
-            TILES = _has_TILES(_get_mtface_flag(search_obj.data)) # type: bool
-            LIGHT = _has_LIGHT(_get_mtface_flag(search_obj.data)) # type: bool
+            # PANEL isn't a button but a fact that
+            TEX = _has_TEX(_get_mtface_flag(search_obj.data)) or "tex" in search_obj.name # type: bool
+            ISCOCKPIT = any(
+                        [root_object.xplane.layer.name.lower().endswith(cockpit_suffix)
+                         for cockpit_suffix in
+                            ["_cockpit.obj",
+                             "_cockpit_inn.obj",
+                             "_cockpit_out.obj"]
+                        ]
+                    ) # type: bool
+            ISPANEL = ISCOCKPIT # type: bool
+            TILES = _has_TILES(_get_mtface_flag(search_obj.data)) or "tiles" in search_obj.name # type: bool, HACK
+            LIGHT = _has_LIGHT(_get_mtface_flag(search_obj.data)) or "light" in search_obj.name # type: bool, HACK
             INVISIBLE = mat.game_settings.invisible # type: bool
-            COLLISION = mat.game_settings.physics # type: bool
-            TWOSIDE   = not mat.game_settings.use_backface_culling # type: bool
+            DYNAMIC   = not mat.game_settings.physics # type: bool
+            TWOSIDE   = mat.game_settings.use_backface_culling # type: bool
             SHADOW    = mat.game_settings.face_orientation == "SHADOW" # type: bool
             ALPHA     = mat.game_settings.alpha_blend == "ALPHA" # type: bool
             CLIP      = mat.game_settings.alpha_blend == "CLIP" # type: bool
             print(
 """
 TEX:       {TEX}
-PANEL:     {PANEL}
+ISCOCKPIT  {ISCOCKPIT}
+ISPANEL:   {ISPANEL}
 TILES:     {TILES}
 LIGHT:     {LIGHT}
 INVISIBLE: {INVISIBLE}
-COLLISION: {COLLISION}
+DYNAMIC:   {DYNAMIC}
 TWOSIDE:   {TWOSIDE}
 SHADOW:    {SHADOW}
 ALPHA:     {ALPHA}
 CLIP:      {CLIP}
 """.format(
         TEX=TEX,
-        PANEL=PANEL,
+        ISCOCKPIT=ISCOCKPIT,
+        ISPANEL=ISPANEL,
         TILES=TILES,
         LIGHT=LIGHT,
         INVISIBLE=INVISIBLE,
-        COLLISION=COLLISION,
+        DYNAMIC=DYNAMIC, #AKA COLLISION
         TWOSIDE=TWOSIDE,
         SHADOW=SHADOW,
         ALPHA=ALPHA,
         CLIP=CLIP,
     )
 )
+            #---TEX----------------------------------------------------------
+            #TODO: Can't capture yet
+            if TEX:
+                if ALPHA:
+                    if (xplane_249_helpers.find_property_in_parents(search_obj, "ATTR_shadow_blend")):
+                        mat.xplane.blend_v1000 = "shadow"
+                        mat.xplane.blendRatio = 0.5
+                        logger.info("{}: Blend Mode='Shadow' and Blend Ratio=0.5".format(mat.name))
+                    if (xplane_249_helpers.find_property_in_parents(search_obj, "GLOBAL_shadow_blend")):
+                        mat.xplane.blend_v1000 = "shadow"
+                        mat.xplane.blendRatio = 0.5
+                        root_object.xplane.layer.export_type = xplane_constants.EXPORT_TYPE_INSTANCED_SCENERY
+                        logger.info("{}: Blend Mode='Shadow' and Blend Ratio=0.5, now Instanced Scenery".format(mat.name))
+                if CLIP:
+                    if (xplane_249_helpers.find_property_in_parents(search_obj, "ATTR_no_blend")):
+                        mat.xplane.blend_v1000 = "off"
+                        mat.xplane.blendRatio = 0.5
+                        logger.info("{}: Blend Mode='Off' and Blend Ratio=0.5".format(mat.name))
+                    if (xplane_249_helpers.find_property_in_parents(search_obj, "GLOBAL_no_blend")):
+                        mat.xplane.blend_v1000 = "off"
+                        mat.xplane.blendRatio = 0.5
+                        root_object.xplane.layer.export_type = xplane_constants.EXPORT_TYPE_INSTANCED_SCENERY
+                        logger.info("{}: Blend Mode='Off' and Blend Ratio=0.5, now Instanced Scenery".format(mat.name))
+            #TODO: Haven't done panels yet
+            if TEX and ISPANEL:
+                pass
+
+            if TEX and (not (TILES or LIGHT)) and ISCOCKPIT:
+                mat.xplane.poly_os = 2
+                logger.info("{}: Poly Offset=2".format(mat.name))
 
             #-----------------------------------------------------------------
+            #---TILES/LIGHT---------------------------------------------------
+            #TODO: Can't capture yet
+            if ((TILES
+                or LIGHT)
+                and xplane_249_helpers.find_property_in_parents(search_obj, "ATTR_draped")):
+                mat.xplane.draped = True
+                logger.info("{}: Draped=True".format(mat.name))
 
+            #---INVISIBLE-----------------------------------------------------
+            if INVISIBLE:
+                mat.xplane.draw = False
+                logger.info("{}: Draw Linked Objects=False".format(mat.name))
+            #-----------------------------------------------------------------
 
+            #---DYNAMIC-------------------------------------------------------
+            if not INVISIBLE and not ISCOCKPIT and DYNAMIC: #TODO: And """not self.iscockpit"""
+                mat.xplane.solid_camera = True
+                logger.info("{}: Solid Camera=True".format(mat.name))
+            #-----------------------------------------------------------------
 
-"""
-            if TEX:
-                prop, _ = xplane_249_helpers.find_property_in_parents(search_obj, "ATTR_shadow_blend", default=0.5)
-                if prop:
-                    mat.xplane.blend_mode = "shadow"
-                else:
-                    prop, _ = xplane_249_helpers.find_property_in_parents(search_obj, "GLOBAL_shadow_blend", default=0.5)
-                    if prop:
-                        mat.xplane.blend_mode = "shadow" # Instanced Mode turns this into GLOBAL_
-                    else:
-                        mat.xplane.blend_mode = "blend"
-                if f.transp == Mesh.FaceTranspModes.CLIP:
-                    if xplane_249_helpers.find_property_in_parents(search_obj,'ATTR_no_blend'):
-                        face.alpha=[Prim.TEST,round(float(get_prop(object,'ATTR_no_blend',0.5)),2)]
-                    elif has_prop(object,'GLOBAL_no_blend'):
-                        face.alpha=[Prim.TEST,round(float(get_prop(object,'GLOBAL_no_blend',0.5)),2)]
-                    else:
-                        face.alpha=[Prim.TEST,0.5]
-"""
+            #---TWOSIDE-------------------------------------------------------
+            if TWOSIDE:
+                logger.info("{}: Two Sided deprecated, skipping".format(mat.name))
+                pass
+            #-----------------------------------------------------------------
+
+            #---SHADOW--------------------------------------------------------
+            if SHADOW:
+                #TODO: Implement Shadow mat.xplane.shadow = True
+                #logger.info("{}: Shadow=True".format(mat.name))
+                logger.info("{}: ATTR_shadow not yet implemented, skipping".format(mat.name))
+            #-----------------------------------------------------------------
 
