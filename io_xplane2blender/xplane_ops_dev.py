fd156974513710c08e72c8efab5d7e2914cfdcd7
tngreene <theodorengreene@gmail.com>
2019-05-24 13:41:11 -0400

    New operator, cleans up xplane_ops and adds a new dev operator to quickly rename root objects

--- io_xplane2blender/xplane_ops_dev.py
+++ io_xplane2blender/xplane_ops_dev.py
@@ -14,7 +14,7 @@ class SCENE_OT_dev_create_lights_txt_summary(bpy.types.Operator):
     bl_label = "Create lights.txt Summary"
     bl_idname = 'scene.dev_create_lights_txt_summary'
     bl_description = "Create a text block listing all known lights and attributes about them"
-    
+
     def execute(self,context):
         xplane_lights_txt_parser.parse_lights_file()
         # Use an internal text file called "Manipulator Type Differeces
@@ -24,7 +24,7 @@ class SCENE_OT_dev_create_lights_txt_summary(bpy.types.Operator):
         else:
             text_file = bpy.data.texts[filename]
             text_file.clear()
-        
+
         named_lights = []
         param_lights = []
         other_lights = []
@@ -39,7 +39,7 @@ class SCENE_OT_dev_create_lights_txt_summary(bpy.types.Operator):
                     other_lights.append(overload)
                 else:
                     named_lights.append(overload)
-        
+
         text_file.write("Named Lights\n")
         text_file.write("------------\n")
         for named_light in named_lights:
@@ -55,42 +55,34 @@ class SCENE_OT_dev_create_lights_txt_summary(bpy.types.Operator):
         text_file.write("------------\n")
         for other_light in other_lights:
             text_file.write("%s\n" % other_light.light_name)
-        
+
         return {'FINISHED'}
-   
-#class SCENE_OT_dev_export_to_current_dir(bpy.types.Operator):
-#    bl_label  = 'Export .blend file to current dir'
-#    bl_idname = 'scene.dev_export_to_current_dir'
-#    bl_description = 'Exports blender file to current working directory. Useful for quick plugin testing'
-#
-#    def execute(self, context):
-#        bpy.ops.export.xplane_obj(filepath=self.initial_dir, export_is_relative=True)
-#        return {'FINISHED'}
+
 
 class SCENE_OT_dev_layer_names_from_objects(bpy.types.Operator):
     bl_label = 'Create Layer Names from Objects'
     bl_idname = 'scene.dev_layer_names_from_objects'
     bl_description = 'Create layer names from objects, stripping Cube_ and Empty_ and prepending "test_" to them'
-    
+
     name_prefix = "test_"
     clean_data_block_string = True
-    
+
     def execute(self,context):
         for layer in bpy.context.scene.xplane.layers:
             layer.name = ""
-        
+
         objects = bpy.context.scene.objects
         xplane_layers = bpy.context.scene.xplane.layers
         for object in sorted(objects.keys()):
             if objects[object].parent != None:
                 continue
-            
+
             cleaned_name = objects[object].name
             if self.clean_data_block_string:
                 m = re.match("(Cube_|Empty_)(.*)", objects[object].name)
                 if m != None:
                     cleaned_name = m.group(2)
-                
+
             #Find first true
             idx = 0
             for obj_layer_idx in objects[object].layers:
@@ -102,11 +94,28 @@ class SCENE_OT_dev_layer_names_from_objects(bpy.types.Operator):
             xplane_layers[idx].name = self.name_prefix + cleaned_name
         return {'FINISHED'}
 
+
+class SCENE_OT_dev_root_names_from_objects(bpy.types.Operator):
+    bl_label = "Create Layer Names"
+    bl_idname = "scene.dev_root_names_from_objects"
+    bl_description = "Creates root object names from root objects, prepending 'test_'"
+
+    name_prefix = "test_"
+
+    def execute(self, context):
+        for ob in context.scene.objects:
+            if not ob.xplane.isExportableRoot:
+                continue
+            else:
+                ob.xplane.layer.name = self.name_prefix + ob.name
+        return {'FINISHED'}
+
+
 class SCENE_OT_dev_rerun_updater(bpy.types.Operator):
     bl_label = "Re-run Updater"
     bl_idname = "scene.dev_rerun_updater"
     bl_description = "Re-runs the updater. This does not undo an update that happened on load!"
-   
+
     def execute(self,context):
         logger = xplane_helpers.logger
         logger.clear()
