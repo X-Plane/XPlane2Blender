c4916595f2b4b16827028214795b46583baff04b
tngreene <theodorengreene@gmail.com>
2019-08-28 10:47:17 -0400

    Adds new "not_applied" test. Again, TDD done right this time!

--- tests/converter/materials/texture_autodetect_applied.test.py
+++ tests/converter/materials/texture_autodetect_applied.test.py
@@ -15,57 +15,57 @@ class TestTextureAutodetectApplied(XPlaneTestCase):
     def test_OBJCustomLightTexSkip(self)->None:
         bpy.ops.xplane.do_249_conversion(workflow_type=WorkflowType.BULK.name)
         root_object = bpy.data.objects[inspect.stack()[0].function[5:]]
-        self.assertTrue(root_object.xplane.autodetectTextures)
-        self.assertEqual(root_object.xplane.texture,"")
-        self.assertEqual(root_object.xplane.texture_lit,"")
-        self.assertEqual(root_object.xplane.texture_normal,"")
-        self.assertEqual(root_object.xplane.texture_draped,"")
-        self.assertEqual(root_object.xplane.texture_draped_normal,"")
+        self.assertTrue(root_object.xplane.layer.autodetectTextures)
+        self.assertEqual(root_object.xplane.layer.texture,"")
+        self.assertEqual(root_object.xplane.layer.texture_lit,"")
+        self.assertEqual(root_object.xplane.layer.texture_normal,"")
+        self.assertEqual(root_object.xplane.layer.texture_draped,"")
+        self.assertEqual(root_object.xplane.layer.texture_draped_normal,"")
 
     def test_OBJFindTextures(self)->None:
         bpy.ops.xplane.do_249_conversion(workflow_type=WorkflowType.BULK.name)
         root_object = bpy.data.objects[inspect.stack()[0].function[5:]]
         tex_used = r"//tex\texture"
-        self.assertFalse(root_object.xplane.autodetectTextures)
-        self.assertEqual(root_object.xplane.texture,        tex_used + ".png")
-        self.assertEqual(root_object.xplane.texture_lit,    tex_used + "_NML.png")
-        self.assertEqual(root_object.xplane.texture_normal, tex_used + "_LIT.png")
-        self.assertEqual(root_object.xplane.texture_draped,"")
-        self.assertEqual(root_object.xplane.texture_draped_normal,"")
+        self.assertFalse(root_object.xplane.layer.autodetectTextures)
+        self.assertEqual(root_object.xplane.layer.texture,        tex_used + ".png")
+        self.assertEqual(root_object.xplane.layer.texture_lit,    tex_used + "_NML.png")
+        self.assertEqual(root_object.xplane.layer.texture_normal, tex_used + "_LIT.png")
+        self.assertEqual(root_object.xplane.layer.texture_draped,"")
+        self.assertEqual(root_object.xplane.layer.texture_draped_normal,"")
 
     def test_OBJFindTexturesDDS(self)->None:
         bpy.ops.xplane.do_249_conversion(workflow_type=WorkflowType.BULK.name)
         root_object = bpy.data.objects[inspect.stack()[0].function[5:]]
         tex_used = r"//tex\textureB"
-        self.assertFalse(root_object.xplane.autodetectTextures)
-        self.assertEqual(root_object.xplane.texture,        tex_used + ".dds")
-        self.assertEqual(root_object.xplane.texture_normal, tex_used + "_NML.dds")
-        self.assertEqual(root_object.xplane.texture_draped,"")
-        self.assertEqual(root_object.xplane.texture_draped_normal,"")
+        self.assertFalse(root_object.xplane.layer.autodetectTextures)
+        self.assertEqual(root_object.xplane.layer.texture,        tex_used + ".dds")
+        self.assertEqual(root_object.xplane.layer.texture_normal, tex_used + "_NML.dds")
+        self.assertEqual(root_object.xplane.layer.texture_draped,"")
+        self.assertEqual(root_object.xplane.layer.texture_draped_normal,"")
 
     def test_OBJFindTexturesDraped(self)->None:
         bpy.ops.xplane.do_249_conversion(workflow_type=WorkflowType.BULK.name)
         root_object = bpy.data.objects[inspect.stack()[0].function[5:]]
-        self.assertFalse(root_object.xplane.autodetectTextures)
+        self.assertFalse(root_object.xplane.layer.autodetectTextures)
         tex_used = r"//tex\texture"
-        self.assertEqual(root_object.xplane.texture,        tex_used + ".png")
-        self.assertEqual(root_object.xplane.texture_normal, tex_used + "_NML.png")
-        self.assertEqual(root_object.xplane.texture_lit,    tex_used + "_LIT.png")
+        self.assertEqual(root_object.xplane.layer.texture,        tex_used + ".png")
+        self.assertEqual(root_object.xplane.layer.texture_normal, tex_used + "_NML.png")
+        self.assertEqual(root_object.xplane.layer.texture_lit,    tex_used + "_LIT.png")
 
         tex_used_draped = r"//tex/draped.png"
-        self.assertEqual(root_object.xplane.texture_draped,        tex_used_draped + ".png")
-        self.assertEqual(root_object.xplane.texture_draped_normal, tex_used_draped + "_NML.png")
+        self.assertEqual(root_object.xplane.layer.texture_draped,        tex_used_draped + ".png")
+        self.assertEqual(root_object.xplane.layer.texture_draped_normal, tex_used_draped + "_NML.png")
 
     def test_OBJNoPanelTexture(self)->None:
         bpy.ops.xplane.do_249_conversion(workflow_type=WorkflowType.BULK.name)
         root_object = bpy.data.objects[inspect.stack()[0].function[5:]]
         tex_used = r"//tex\texture"
-        self.assertFalse(root_object.xplane.autodetectTextures)
-        self.assertEqual(root_object.xplane.texture,        tex_used + ".png")
-        self.assertEqual(root_object.xplane.texture_lit,    tex_used + "_NML.png")
-        self.assertEqual(root_object.xplane.texture_normal, tex_used + "_LIT.png")
-        self.assertEqual(root_object.xplane.texture_draped,"")
-        self.assertEqual(root_object.xplane.texture_draped_normal,"")
+        self.assertFalse(root_object.xplane.layer.autodetectTextures)
+        self.assertEqual(root_object.xplane.layer.texture,        tex_used + ".png")
+        self.assertEqual(root_object.xplane.layer.texture_lit,    tex_used + "_NML.png")
+        self.assertEqual(root_object.xplane.layer.texture_normal, tex_used + "_LIT.png")
+        self.assertEqual(root_object.xplane.layer.texture_draped,"")
+        self.assertEqual(root_object.xplane.layer.texture_draped_normal,"")
 
 
 runTestCases([TestTextureAutodetectApplied])
---
+++ tests/converter/materials/texture_autodetect_not_applied.test.py
@@ -0,0 +1,26 @@
+import inspect
+
+from typing import Tuple
+import os
+import sys
+
+import bpy
+from io_xplane2blender import xplane_config
+from io_xplane2blender.tests import *
+from io_xplane2blender.xplane_249_converter.xplane_249_constants import WorkflowType
+
+__dirname__ = os.path.dirname(__file__)
+
+class TestTextureAutodetectNotApplied(XPlaneTestCase):
+    def test_all_roots_have_no_autodetect(self)->None:
+        for root in filter(lambda o: o.xplane.isExportableRoot, bpy.data.objects):
+            self.assertTrue(root.xplane.layer.autodetectTextures)
+            self.assertEqual(root.xplane.layer.texture, "")
+            self.assertEqual(root.xplane.layer.texture_normal, "")
+            self.assertEqual(root.xplane.layer.texture_lit, "")
+            self.assertEqual(root.xplane.layer.texture_lit, "")
+            self.assertEqual(root.xplane.layer.texture_draped, "")
+            self.assertEqual(root.xplane.layer.texture_draped_normal, "")
+
+
+runTestCases([TestTextureAutodetectNotApplied])
