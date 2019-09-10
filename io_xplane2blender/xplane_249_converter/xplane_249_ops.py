36b8452c5b9b60dd2294f1e14e77eec9ad7c3ed5
tngreene <theodorengreene@gmail.com>
2019-09-10 13:52:01 -0400

    Adds important assert to ops. I think the whole "'' not found in enum problem has been fixed." where it didn't know that "AIRCRAFT" was a member of ProjectTypes. I don't know why it thought it was a problem.

--- io_xplane2blender/xplane_249_converter/xplane_249_ops.py
+++ io_xplane2blender/xplane_249_converter/xplane_249_ops.py
@@ -10,15 +10,17 @@ class SCENE_OT_249_do_conversion(bpy.types.Operator):
     bl_description = "Convert a file's 2.49 property format to 2.7x. WARNING: Running multiple times may have disastrous consequences"
 
     project_type = bpy.props.StringProperty(
-            name = "Project Type",
-            description="The type of project contain in the blend file, one of " + ", ".join([attr.name for attr in xplane_249_constants.ProjectType])
-            )
+        name = "Project Type",
+        description="The type of project contain in the blend file, one of " + ", ".join([attr.name for attr in xplane_249_constants.ProjectType])
+        )
     workflow_type = bpy.props.StringProperty(
         name="Workflow Type",
         description="The workflow used in 2.49, one of " + ", ".join([attr.name for attr in xplane_249_constants.WorkflowType]),
         )
 
     def execute(self, context: bpy.types.Context):
+        assert self.project_type in xplane_249_constants.ProjectType.__dict__, \
+                "{} is not a known ProjectType".format(self.project_type)
         assert self.workflow_type in xplane_249_constants.WorkflowType.__dict__, \
                 "{} is not a known WorkflowType".format(self.workflow_type)
         xplane_249_convert.do_249_conversion(
