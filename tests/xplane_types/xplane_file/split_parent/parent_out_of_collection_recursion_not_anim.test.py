c8a233cbc6317991954a82a2d61a77e4e468a8f0
tngreene <theodorengreene@gmail.com>
2019-12-04 16:23:53 -0500

    The find_anim case has been split into two to avoid the multiple scene issue in the unit tester. _not_anim has the cases where a not really animated parent isn't used

---
+++ tests/xplane_types/xplane_file/split_parent/parent_out_of_collection_recursion_not_anim.test.py
@@ -0,0 +1,30 @@
+import inspect
+
+from typing import Tuple
+import os
+import sys
+
+import bpy
+from io_xplane2blender import xplane_config, xplane_helpers
+from io_xplane2blender.tests import *
+
+__dirname__ = os.path.dirname(__file__)
+
+def filterLines(line:Tuple[str])->bool:
+    return (isinstance(line[0],str)
+             and ("ANIM_" in line[0]
+                  or "TRIS" in line[0]))
+
+
+class ParentOutOfCollectionNotAnim(XPlaneTestCase):
+    def test_ExpCollection_not_really_animated(self)->None:
+        filename = inspect.stack()[0].function
+
+        self.assertRootObjectExportEqualsFixture(
+            bpy.data.collections[filename[5:]],
+            os.path.join(__dirname__, "fixtures", filename + ".obj"),
+            filename,
+            filterLines
+        )
+
+runTestCases([ParentOutOfCollectionNotAnim])
