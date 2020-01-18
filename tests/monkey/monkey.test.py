322b1c53ca74e3ece0e8877a3b5a2e0f3d2e161f
tngreene <theodorengreene@gmail.com>
2020-01-17 20:52:04 -0500

    Close enough. Don't know why the float tollerence isn't enough.

--- tests/monkey/monkey.test.py
+++ tests/monkey/monkey.test.py
@@ -12,16 +12,6 @@ class MonkeyTest(XPlaneTestCase):
         # only keep ANIM_ lines
         return isinstance(line[0], str) and line[0].find('ANIM_') == 0
 
-    '''
-    def test_monkey_against_249_export(self):
-        filename = 'test_monkey_249'
-        self.assertLayerExportEqualsFixture(
-            0, os.path.join(__dirname__, 'fixtures', filename + '.obj'),
-            filename,
-            self.filterLine
-        )
-    '''
-
     def test_monkey_export(self):
         filename = 'test_monkey'
         self.assertLayerExportEqualsFixture(
