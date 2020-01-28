f1d2eecd896fc1f499d619883b26ac8e5a5bf3f4
tngreene <theodorengreene@gmail.com>
2020-01-28 14:16:00 -0500

    create_from_root_objects tests (a bunch of weird stuff) but also LODs, meaning we had to move it to 2.80 and set the checkboxes This one was so old it wasn't even using a filterLines function.

--- tests/xplane_types/xplane_file/create_from_root_objects.test.py
+++ tests/xplane_types/xplane_file/create_from_root_objects.test.py
@@ -7,6 +7,14 @@ from io_xplane2blender import xplane_config
 
 __dirname__ = os.path.dirname(__file__)
 
+def filterLines(line:Tuple[str])->bool:
+    return (isinstance(line[0], str)
+            and any(d in line[0]
+                    for d in {"ATTR_LOD",
+                              "ATTR_shiny_rat",
+                              "TRIS",
+                              "TEXTURE"}))
+
 class TestCreateFromRootObjects(XPlaneTestCase):
     def test_create_files_from_root_objects(self):
         tmpDir = os.path.realpath(os.path.join(__dirname__, '../../tmp'))
@@ -15,9 +23,8 @@ class TestCreateFromRootObjects(XPlaneTestCase):
 
         out = xplaneFile.write()
 
-        fh = open(os.path.join(tmpDir, 'test_export_root_objects_1.obj'), 'w')
-        fh.write(out)
-        fh.close()
+        with open(os.path.join(tmpDir, 'test_export_root_objects_1.obj'), 'w') as fh:
+            fh.write(out)
 
         # auto filename from blender object
         self.assertEqual(xplaneFile.filename, 'root_1')
@@ -25,15 +32,33 @@ class TestCreateFromRootObjects(XPlaneTestCase):
         # should contain 3 cubes
         self.assertEqual(len(xplaneFile._bl_obj_name_to_bone), 4)
 
-        self.assertObjectsInXPlaneFile(
-            xplaneFile, [
-            'root_1',
-            'root_1_child_1',
-            'root_1_child_1_child',
-            'root_1_child_2'
-        ])
+        def assertXplaneFileHasBoneTree(self, xplaneFile, tree):
+            self.assertIsNotNone(xplaneFile.rootBone)
 
-        self.assertXplaneFileHasBoneTree(
+            bones = []
+
+            def collect(bone):
+                bones.append(bone)
+                for bone in bone.children:
+                    collect(bone)
+
+            collect(xplaneFile.rootBone)
+
+            self.assertEqual(len(tree), len(bones))
+
+            index = 0
+
+            while index < len(bones):
+                self.assertEqual(tree[index], bones[index].getName())
+                index += 1
+        self.assertIn('root_1', xplaneFile._bl_obj_name_to_bone)
+
+        self.assertIn('root_1_child_1', xplaneFile._bl_obj_name_to_bone)
+        self.assertIn('root_1_child_1_child', xplaneFile._bl_obj_name_to_bone)
+        self.assertIn('root_1_child_2', xplaneFile._bl_obj_name_to_bone)
+
+        assertXplaneFileHasBoneTree(
+            self,
             xplaneFile, [
             '0 Mesh: root_1',
                 '1 Mesh: root_1_child_1',
@@ -43,16 +68,16 @@ class TestCreateFromRootObjects(XPlaneTestCase):
 
         self.assertFileOutputEqualsFixture(
             out,
-            os.path.join(__dirname__, 'fixtures',  'test_export_root_objects_1.obj')
+            os.path.join(__dirname__, 'fixtures',  'test_export_root_objects_1.obj'),
+            filterLines
         )
 
         xplaneFile2 = xplane_file.createFileFromBlenderRootObject(bpy.data.objects['root_2'])
 
         out = xplaneFile2.write()
 
-        fh = open(os.path.join(tmpDir, 'test_export_root_objects_2.obj'), 'w')
-        fh.write(out)
-        fh.close()
+        with  open(os.path.join(tmpDir, 'test_export_root_objects_2.obj'), 'w') as fh:
+            fh.write(out)
 
         # custom file name
         self.assertEqual(xplaneFile2.filename, 'custom_name')
@@ -60,19 +85,18 @@ class TestCreateFromRootObjects(XPlaneTestCase):
         # should contain 1 cube
         self.assertEqual(len(xplaneFile2._bl_obj_name_to_bone), 1)
 
-        self.assertObjectsInXPlaneFile(
-            xplaneFile2, [
-            'root_2'
-        ])
+        self.assertIn("root_2", xplaneFile2._bl_obj_name_to_bone)
 
-        self.assertXplaneFileHasBoneTree(
+        assertXplaneFileHasBoneTree(
+            self,
             xplaneFile2, [
             '0 Mesh: root_2'
         ])
 
         self.assertFileOutputEqualsFixture(
             out,
-            os.path.join(__dirname__, 'fixtures', 'test_export_root_objects_2.obj')
+            os.path.join(__dirname__, 'fixtures', 'test_export_root_objects_2.obj'),
+            filterLines
         )
 
 runTestCases([TestCreateFromRootObjects])
