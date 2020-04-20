00c7cc4704538dbe1530c26cbd2bce382bef7179
tngreene <theodorengreene@gmail.com>
2020-04-20 11:05:40 -0400

    Closes #325!

--- io_xplane2blender/xplane_types/xplane_light.py
+++ io_xplane2blender/xplane_types/xplane_light.py
@@ -132,8 +132,8 @@ class XPlaneLight(xplane_object.XPlaneObject):
             self.comment = "".join(params_itr).lstrip()
 
             if len(params_actual) < len(params_formal):
-                logger.error(f"Not enough actual parameters ({' '.join(params_actual)}) to"
-                             f" satisfy LIGHT_PARAM_DEF {len(params_formal)} {' '.join(params_formal)}")
+                logger.error(f"Not enough actual parameters ('{' '.join(params_actual)}') to"
+                             f" satisfy 'LIGHT_PARAM_DEF {len(params_formal)} {' '.join(params_formal)}'")
                 return
 
             if self.comment and not self.comment.startswith(("//","#")):
--- tests/lights/param_light_params.test.py
+++ tests/lights/param_light_params.test.py
@@ -11,6 +11,7 @@ __dirname__ = os.path.dirname(__file__)
 
 class TestParamLightParams(XPlaneTestCase):
     def test_comment_correct(self):
+        filename = inspect.stack()[0].function
         xplaneFile = self.createXPlaneFileFromPotentialRoot("Layer 1")
         light = xplaneFile._bl_obj_name_to_bone["04_comment_contents_preserved_exactly"].xplaneObject
         self.assertEqual(light.comment, "0 spaces, number starts comment with uneven and a   trailing  space ")
@@ -19,9 +20,19 @@ class TestParamLightParams(XPlaneTestCase):
         out = self.exportLayer(1)
         self.assertLoggerErrors(3)
 
-    def test_UNUSED_and_special_case_params(self):
-        out = self.exportLayer(2)
-        #self.assertLoggerErrors(3)
+    def test_unused_param_pass(self):
+        filename = inspect.stack()[0].function
+
+        self.assertExportableRootExportEqualsFixture(
+            filename[5:],
+            os.path.join(__dirname__, "fixtures", f"{filename}.obj"),
+            {"LIGHT_PARAM"},
+            filename,
+        )
+
+    def test_unused_param_fail(self):
+        out = self.exportLayer(3)
+        self.assertLoggerErrors(2)
 
 runTestCases([TestParamLightParams])
 
