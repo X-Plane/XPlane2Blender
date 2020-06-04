ffba1af6aac0d0c108b3f351be8bd216617f2dee
tngreene <theodorengreene@gmail.com>
2020-06-04 17:49:53 -0400

    Another unit test, a little redundant, for just testing that our properties we use are all used when they're not set to their defaults

---
+++ tests/lights/automatic_detect/automatic_light_properties.test.py
@@ -0,0 +1,27 @@
+import inspect
+
+from typing import Tuple
+import os
+import sys
+
+import bpy
+from io_xplane2blender import xplane_config
+from io_xplane2blender.tests import *
+from io_xplane2blender.tests import test_creation_helpers
+
+__dirname__ = os.path.dirname(__file__)
+
+class TestAutomaticLightProperties(XPlaneTestCase):
+    #TI as per unittest requirements, all test methods must start with "test_"
+    def test_automatic_light_properties(self)->None:
+        filename = inspect.stack()[0].function
+
+        self.assertExportableRootExportEqualsFixture(
+            filename[5:],
+            os.path.join(__dirname__, "fixtures", f"{filename}.obj"),
+            {"ANIM", "LIGHT"},
+            filename,
+        )
+
+
+runTestCases([TestAutomaticLightProperties])
