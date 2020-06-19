2a10b43791d7252f8bb480fc7cb10424e4518425
tngreene <theodorengreene@gmail.com>
2020-06-19 12:00:52 -0400

    Last unit test of the feature! Test that animation actually works instead of assuming that just because we just the bake matrix we're good. Well, we are good and it looks really funny in X-Plane

---
+++ tests/lights/automatic_detect/automatic_parent_and_self_animated.test.py
@@ -0,0 +1,37 @@
+import inspect
+
+from typing import Tuple
+import os
+import sys
+
+import bpy
+from io_xplane2blender import xplane_config
+from io_xplane2blender.tests import *
+from io_xplane2blender.tests import test_creation_helpers
+
+__dirname__ = os.path.dirname(__file__)
+
+
+class TestAutomaticParentAndSelfAnimated(XPlaneTestCase):
+    def test_self_animated_lights(self)->None:
+        filename = inspect.stack()[0].function
+
+        self.assertExportableRootExportEqualsFixture(
+            filename[5:],
+            os.path.join(__dirname__, "fixtures", f"{filename}.obj"),
+            {"ANIM", "LIGHT"},
+            filename,
+        )
+
+    def test_parent_animated_lights(self)->None:
+        filename = inspect.stack()[0].function
+
+        self.assertExportableRootExportEqualsFixture(
+            filename[5:],
+            os.path.join(__dirname__, "fixtures", f"{filename}.obj"),
+            {"ANIM", "LIGHT"},
+            filename,
+        )
+
+
+runTestCases([TestAutomaticParentAndSelfAnimated])
